b36f19ae2dfb4b9014ea917246767bb349a8ed08
Atulya Verma <atulyaverma7068@gmail.com>
2025-07-30 18:30:05 +0530

    project structure ready, env ready

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Atulya Verma",
+    author_email="atulyaverma7068@gmail.com",
+    packages=find_packages()
+)
